a62ba60ad156aa21ae6fd1599a3156bb9bef40f6
Wisdom Mpamugo <wisdommpam99@gmail.com>
2025-04-19 01:33:44 +0100

    19/04

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://pleased-hermit-75.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ src/app/generate-program/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GenerateProgramPage = () => {
+  return (
+    <div>
+      Generate Program
+    </div>
+  )
+}
+
+export default GenerateProgramPage
